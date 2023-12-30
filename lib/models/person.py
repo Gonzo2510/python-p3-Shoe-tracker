9575ce5fdf0da00e1f87bdf42710b23e121ef00f
Jacob Gonzales <Jagonz@live.com>
2023-12-30 12:00:31 -0600

    added name property

--- lib/models/person.py
+++ lib/models/person.py
@@ -11,3 +11,16 @@ class Person:
 
     def __repr__(self) -> str:
         return f"<Person {self.id}: {self.name}>"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
