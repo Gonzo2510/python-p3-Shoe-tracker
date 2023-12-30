d32ce25e63bfdeb8987a52a563d7485a8255625c
Jacob Gonzales <Jagonz@live.com>
2023-12-30 11:56:38 -0600

    create Person class

--- lib/models/person.py
+++ lib/models/person.py
@@ -1,2 +1,13 @@
 from models.__init__ import CURSOR, CONN
 from models.shoe import Shoe
+
+class Person:
+
+    all ={}
+
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"<Person {self.id}: {self.name}>"
