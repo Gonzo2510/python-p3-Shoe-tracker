38a93daaeffdf0ca89bd21c16ce71dcc344f1190
Jacob Gonzales <Jagonz@live.com>
2023-12-30 11:37:14 -0600

    add imports to cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -3,6 +3,15 @@
 from helpers import (
     exit_program,
     helper_1,
+    create_person,
+    update_person,
+    delete_person,
+    list_persons,
+    find_person_by_id,
+    create_shoe,
+    update_shoe,
+    delete_shoe,
+    list_shoes
 )
 
 
@@ -14,6 +23,30 @@ def main():
             exit_program()
         elif choice == "1":
             helper_1()
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        elif choice == "7":
+            pass
+        elif choice == "8":
+            pass
+        elif choice == "9":
+            pass
+        elif choice == "10":
+            pass
+        elif choice == "11":
+            pass
+        elif choice == "12":
+            pass
+        elif choice == "13":
+            pass
         else:
             print("Invalid choice")
 
