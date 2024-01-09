568ff15fe3fca758f05283019981b60c164eb0cf
Jacob Gonzales <Jagonz@live.com>
2024-01-09 10:03:39 -0600

    update numbering on cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,6 @@
 
 from helpers import (
     exit_program,
-    helper_1,
     create_person,
     update_person,
     delete_person,
@@ -22,31 +21,23 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
-        elif choice == "2":
             create_person()
-        elif choice == "3":
+        elif choice == "2":
             update_person()
-        elif choice == "4":
+        elif choice == "3":
             delete_person()
-        elif choice == "5":
+        elif choice == "4":
             list_persons()
-        elif choice == "6":
+        elif choice == "5":
             find_person_by_id()
-        elif choice == "7":
+        elif choice == "6":
             create_shoe()
-        elif choice == "8":
+        elif choice == "7":
             update_shoe()
-        elif choice == "9":
+        elif choice == "8":
             delete_shoe()
-        elif choice == "10":
+        elif choice == "9":
             list_shoes()
-        elif choice == "11":
-            pass
-        elif choice == "12":
-            pass
-        elif choice == "13":
-            pass
         else:
             print("Invalid choice")
 
@@ -54,7 +45,16 @@ def main():
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. Create person")
+    print("2. Update person")
+    print("3. Delete person")
+    print("4. List persons")
+    print("5. Finder person by id")
+    print("6. Create shoe")
+    print("7. Update shoe")
+    print("8. Delete shoe")
+    print("9. List shoes")
+
 
 
 if __name__ == "__main__":
