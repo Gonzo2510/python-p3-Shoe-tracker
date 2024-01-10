7a987f357433061131499acfed585375772c2c61
Jacob Gonzales <Jagonz@live.com>
2024-01-09 22:55:00 -0600

    update class methods for shoe.py

--- lib/models/shoe.py
+++ lib/models/shoe.py
@@ -70,20 +70,52 @@ class Shoe:
         CONN.commit()
 
     def update(self):
-        pass
+        sql = """
+            UPDATE shoes
+            SET brand = ?, size = ?, type = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.brand, self.size, self.type, self.id))
+        CONN.commit()
 
     def delete(self):
-        pass
+        sql = """
+            DELETE FROM shoes
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        CONN.commit()
+
+        del type(self).all[self.id]
+        self.id = None
 
 
     @classmethod
-    def create():
-        pass
+    def create(cls, brand, size, type):
+        shoe = cls(brand, size, type)
+        shoe.save()
+        return shoe
 
     @classmethod
-    def instance_from_db():
-        pass
+    def instance_from_db(cls, row):
+        shoe = cls.all.get(row[0])
+        if shoe:
+            shoe.brand = row[1]
+            shoe.size = row[2]
+            shoe.type = row[3]
+        else:
+            shoe = cls(row[1], row[2], row[3])
+            shoe.id = row[0]
+            cls.all[shoe.id] = shoe
+        return shoe
+        
 
     @classmethod
     def get_all(cls):
-        pass
+        sql = """
+            SELECT *
+            FROM shoes
+        """
+        rows = CURSOR.execute(sql).fetchall()
+
+        return[cls.instance_from_db(row) for row in rows]
