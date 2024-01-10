2ea6884e66ba78f3cd75c568bf30d5260f85774c
Jacob Gonzales <Jagonz@live.com>
2024-01-10 11:10:39 -0600

    person.py add find_by_id func

--- lib/models/person.py
+++ lib/models/person.py
@@ -112,4 +112,13 @@ class Person:
 
         return[cls.instance_from_db(row) for row in rows]
     
-    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT *
+            FROM persons
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
