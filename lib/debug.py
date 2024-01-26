fd5753b692c478523cf964c92b7a3a67c18cf373
Jacob Gonzales <Jagonz@live.com>
2024-01-26 11:15:34 -0600

    move reset and seed_database to debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,32 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
-import ipdb
+from models.owner import Owner
+from models.shoe import Shoe
 
+def reset_database():
+    Shoe.drop_table()
+    Owner.drop_table()
+    Owner.create_table()
+    Shoe.create_table()
+    print("Database cleared")
 
-ipdb.set_trace()
+def seed_database():
+    Owner.create("Jacob")
+    Owner.create("Isabel")
+    Owner.create("Aaron")
+    Owner.create("Joe")
+
+    Shoe.create("Puma", 8, 2)
+    Shoe.create("Nike", 13, 1)
+    Shoe.create("Reebok", 12, 3)
+    Shoe.create("Adidas", 11, 4)
+    Shoe.create("Converse", 8, 2)
+    Shoe.create("Under Armour", 8, 2)
+    Shoe.create("Vans", 8, 2)
+    Shoe.create("Jordan", 8, 2)
+    Shoe.create("Nike", 8, 2)
+
+    print("Database seeded")
+
+breakpoint()
--- lib/helpers.py
+++ lib/helpers.py
@@ -112,28 +112,3 @@ def list_shoes_by_owner_id():
         owner_shoes = owner.shoes()
         for shoe in owner_shoes:
             print(shoe)
-
-def create_clear_database():
-    Shoe.drop_table()
-    Owner.drop_table()
-    Owner.create_table()
-    Shoe.create_table()
-    print("Database cleared")
-
-def seed_database():
-    Owner.create("Jacob")
-    Owner.create("Isabel")
-    Owner.create("Aaron")
-    Owner.create("Joe")
-
-    Shoe.create("Puma", 8, 2)
-    Shoe.create("Nike", 13, 1)
-    Shoe.create("Reebok", 12, 3)
-    Shoe.create("Adidas", 11, 4)
-    Shoe.create("Converse", 8, 2)
-    Shoe.create("Under Armour", 8, 2)
-    Shoe.create("Vans", 8, 2)
-    Shoe.create("Jordan", 8, 2)
-    Shoe.create("Nike", 8, 2)
-
-    print("Database seeded")
